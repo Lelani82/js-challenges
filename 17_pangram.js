37da83350b477ff023703b621531a918f31a8e13
lelani82 <lelaniparker@hotmail.com>
2019-11-18 17:55:08 +1000

    17 attempted

--- 17_pangram.js
+++ 17_pangram.js
@@ -10,9 +10,19 @@ If the given string has any uppercase or numbers, it should return false
 */
 
 function isPangram(string) {
-    // Your code here
+    // define the alphabet
+    var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    string = string.split(" ");
+    for (let i = 0; i > letters.length; i++) {
+        if (!string.includes(letters[i])) {
+            return false;
+        }
+    }
+    return true;
+
 }
 
-module.exports = {
-    isPangram
-};
+console.log(isPangram("The quick brown fox jumps over the lazy dog"));
+// module.exports = {
+//     isPangram
+// };
