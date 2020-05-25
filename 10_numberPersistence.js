a74ed4efe27275ffee60b9829973a18a4465ec0e
Lelani Parker <lelaniparker@hotmail.com>
2020-05-25 14:41:51 +1000

    completed challenge 10

--- 10_numberPersistence.js
+++ 10_numberPersistence.js
@@ -13,19 +13,27 @@ Because 9*9*9 = 729, 7*2*9 = 126, 1*2*6 = 12, and 1*2 = 2
 
 function persistence(num) {
     // Continue to split numbers while num <= 9 (until single digit)
-    while (num <= 9) {
+    let total = 1;
+    let count = 0;
+    while (num >= 9) {
         // split number into array of numbers
-        //let arr = Array.from(String(num), Number);
-        //console.log(arr);
+        let arr = Array.from(num.toString()).map(Number);
         // iterate through array & multiply numbers together & count how many times this happens
-        //total = 1;
-        // for (let i = 0; i < arr.length; i++) {
-        //     total *= arr[i];
+        for (let i=0; i <arr.length; i++) {
+            total *= arr[i];
+        }
+        // update count
+        count += 1;
+        // set num to total
+        num = total;
+        // reset total to 1
+        total = 1;
     }
-    //console.log(total);
+    console.log(count);
+    return count;
 }
 
 persistence(39);
-// module.exports = {
-//     persistence
-// }
+module.exports = {
+    persistence
+}
